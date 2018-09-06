896cfb59baf05e8f12ad385b156003cdafd3fb7f
ChanR <chan.rahar@gmail.com>
2018-09-05 20:11:20 -0400

    added different background picture, and improve js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,32 +4,38 @@ $(document).ready(function () {
             question: "This anime follows the story of a young ninja and his dream to become the leader of his village",
             answers: ["Naruto", "One Piece", "Bleach", "My Hero Academia", "Death Note"],
             values: [true, false, false, false, false],
+            picture: "assets/images/naruto.jpg",
         },
         {
             question: "This anime follows the story of a pirate and his journey to be the next Pirate King",
             answers: ["My Hero Academia", "Naruto", "Death Note", "One Piece", "Bleach"],
             values: [false, false, false, true, false],
+            picture: "assets/images/onepiece.jpg",
         },
         {
             question: "This anime follows the adventure of a young man that has acquired the power of a Death God to help his friends",
             answers: ["One Piece", "Death Note", "Bleach", "My Hero Academia", "Naruto"],
             values: [false, false, true, false, false],
+            picture: "assets/images/bleach.jpg",
         },
         {
             question: "This anime follows the adventure of a human that has acquired the a notebook that could kill people just by writing his or her name into the notebook while thinking of the person face",
             answers: ["Death Note", "One Piece", "My Hero Academia", "Bleach", "Naruto"],
             values: [true, false, false, false, false],
+            picture: "assets/images/deathnote.jpg",
         },
         {
             question: "This anime follows the adventure of a young man that had no superpower in a world where everyone is born with superpower",
             answers: ["Bleach", "Naruto", "Death Note", "My Hero Academia", "One Piece"],
             values: [false, false, false, true, false],
+            picture: "assets/images/boku_no_hero_academia.jpg",
         }]
 
     var correct = 0;
     var wrong = 0;
     var none = 0;
     var currentQuestion = 0;
+
 
 
     $("#start").on("click", function () {
@@ -43,7 +49,7 @@ $(document).ready(function () {
         $("#start").remove();
         $("#content").empty();
 
-        var answerFind = questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)];
+        $("body").css("background-image", "url(assets/images/background1.jpg)");
 
         var questionDiv = $("<div>");
         questionDiv.attr("class", "question-Div");
@@ -65,8 +71,9 @@ $(document).ready(function () {
             if (time === 0) {
                 clearInterval(countDown);
                 none++;
-                alert("Timed out. Correct answer is " + answerFind);
-                nextQ();
+                noAns();
+                // alert("Timed out. Correct answer is " + answerFind);
+
             }
         }, 1000);
 
@@ -85,24 +92,71 @@ $(document).ready(function () {
             if ($(this).attr("value") === "true") {
                 correct++;
                 clearInterval(countDown);
-                alert("That is Right");
-                nextQ();
+                rightAns();
+                // alert("That is Right");
+
             } else if ($(this).attr("value") === "false") {
                 wrong++;
                 clearInterval(countDown);
-                alert("That is Wrong. Correct answer is " + answerFind);
-                nextQ();
+                wrongAns();
+                // alert("That is Wrong. Correct answer is " + answerFind);
+
             };
         });
     };
 
-    function nextQ() {
+    function rightAns() {
+        var showAns = setTimeout(questionDis, 5* 1000);
+        $("body").css("background-image", "url(" + questions[currentQuestion].picture + ")");
+        $("#content").empty();
+        var answerDiv = $("<div>");
+        var winAns = $("<h3>");
+        answerDiv.appendTo("#content");
+        winAns.appendTo(answerDiv);
+        winAns.text("That is Right");
+
         if (currentQuestion === (questions.length - 1)) {
-            setTimeout(gameOver);
+            clearTimeout(showAns);
+            setTimeout(gameOver, 5* 1000);
         }
         currentQuestion++;
-        questionDis();
     }
+
+    function wrongAns() {
+        var showAns = setTimeout(questionDis, 5* 1000);
+        $("body").css("background-image", "url(" + questions[currentQuestion].picture + ")");
+        $("#content").empty();
+        var answerDiv = $("<div>");
+        var winAns = $("<h3>");
+        answerDiv.appendTo("#content");
+        winAns.appendTo(answerDiv);
+        winAns.text("That is Incorrect. Correct answer is " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
+
+        if (currentQuestion === (questions.length - 1)) {
+            clearTimeout(showAns);
+            setTimeout(gameOver, 5* 1000);
+        }
+        currentQuestion++;
+    }
+
+    function noAns() {
+        var showAns = setTimeout(questionDis, 5* 1000);
+        $("body").css("background-image", "url(" + questions[currentQuestion].picture + ")");
+        $("#content").empty();
+        var answerDiv = $("<div>");
+        var winAns = $("<h3>");
+        answerDiv.appendTo("#content");
+        winAns.appendTo(answerDiv);
+        winAns.text("TIME OUT. Correct answer is " + questions[currentQuestion].answers[questions[currentQuestion].values.indexOf(true)]);
+
+        if (currentQuestion === (questions.length - 1)) {
+            clearTimeout(showAns);
+            setTimeout(gameOver, 5* 1000);
+        }
+        currentQuestion++;
+
+    }
+
 
     function again() {
         $("#content").empty();
@@ -114,6 +168,8 @@ $(document).ready(function () {
     }
 
     function gameOver() {
+        $("#content").empty();
+        $("body").css("background-image", "url(assets/images/background1.jpg)");
         var scorecard = $("<div>");
         scorecard.attr("class", "question-Div");
         var totalCorrect = $("<h3>");
@@ -132,8 +188,6 @@ $(document).ready(function () {
         restart.addClass("restart");
         restart.text("Restart?");
         $("#content").append(restart);
-
-
 
         var restartTime = setTimeout(again, 10 * 1000);
 
